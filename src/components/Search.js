9770a77404d3cabd90dbc7e1f38170fbad5db194
Kelli Landry <landry.kel@gmail.com>
2020-07-29 17:38:06 -0500

    Scaffolded w/ functions to slice & convert url into numbers since parent component takes only id's that are numbers, set up handleClick for updating state App.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PokeDex from "pokedex-promise-v2";
 import Fuse from "fuse.js";
-import capitalize from "../helpers/stringFormatter";
+import capitalize, { formatURL } from "../helpers/stringFormatter";
 import Styles from "../styles/SearchStyles";
 
 export default function Search({ onSelect }) {
@@ -9,6 +9,8 @@ export default function Search({ onSelect }) {
   const [pokemon, setPokemon] = useState([]);
   const [query, updateQuery] = useState("");
   const [results, setResults] = useState([]);
+
+  // fetches all Pokemon from API, sets pokemon state w/ results
   const getAllPokemon = () => {
     let interval = {
       limit: 10000,
@@ -19,22 +21,26 @@ export default function Search({ onSelect }) {
     );
   };
 
+  // injects ui w/ fetched pokemon
   useEffect(() => {
     getAllPokemon();
   }, [pokemon]);
 
+  // declares instance of fuse search, takes in pokemon by name from fetch in useEffect
   const fuse = new Fuse(pokemon, {
-    keys: ["name"],
+    keys: ["name", "url"],
     includeScore: true,
     includeMatches: true,
     minMatchCharLength: 2,
     threshold: 0.1,
   });
 
+  // updates value as user types within input
   function onSearch(e) {
     updateQuery(e.target.value);
   }
 
+  // will display results from fuse instance & will update as user enters query
   const handleEnter = async () => {
     const foundResults = await fuse.search(query, { limit: 15 });
     const foundArray = query
@@ -42,6 +48,8 @@ export default function Search({ onSelect }) {
       : foundResults;
     setResults(foundArray);
   };
+
+  const handleClick = (pokemon) => {};
 
   return (
     <Styles>
@@ -56,9 +64,17 @@ export default function Search({ onSelect }) {
       <img src={require("../assets/icons/Search.svg")} alt="Search Icon" />
       <div className="container">
         {results
-          ? results.map((p, index) => (
-              <div key={index} className="results-map">
-                <p>{capitalize(p.name)}</p>
+          ? results.map((p) => (
+              <div
+                key={formatURL(p.url)}
+                className="results-map"
+                onClick={() => {
+                  console.log("id", formatURL(p.url));
+                }}
+              >
+                <p>
+                  {capitalize(p.name)} - {formatURL(p.url)}
+                </p>
               </div>
             ))
           : null}
