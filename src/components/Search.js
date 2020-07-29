5e13fcd3432b14330dd908be8a39f0b93d1dc7ef
Kelli Landry <landry.kel@gmail.com>
2020-07-28 20:07:00 -0500

    Add fuse.js as search, displaying all pokemon for now, but will find right results w/ fuzzy search, working on onSelect, and getting it to behave where stats & image change onClick

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PokeDex from "pokedex-promise-v2";
+import Fuse from "fuse.js";
 
-export default function Search() {
+export default function Search({ onSelect }) {
   const P = new PokeDex();
   const [pokemon, setPokemon] = useState([]);
-  const [value, setValue] = useState("");
+  const [query, updateQuery] = useState("");
   const getAllPokemon = () => {
     let interval = {
       limit: 10000,
@@ -19,9 +20,35 @@ export default function Search() {
     getAllPokemon();
   }, [pokemon]);
 
+  const fuse = new Fuse(pokemon, {
+    keys: ["name"],
+    includeScore: true,
+  });
+
+  const results = fuse.search(query);
+  const pokeResults = query ? results.map((pokemon) => pokemon.item) : pokemon;
+
+  function onSearch(e) {
+    updateQuery(e.target.value);
+  }
+
   return (
     <div>
-      <input type="search" placeholder="Search Pokedex..." value={value} />
+      <input
+        type="text"
+        placeholder="Search Pokedex..."
+        value={query}
+        onChange={onSearch}
+      />
+      <div>
+        {pokeResults.map((p, index) => {
+          return (
+            <div key={index} onClick={() => onSelect(p)}>
+              {p.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
